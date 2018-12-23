0d98c9b9aa3cd36ac6fdf52a9b8cd26a6f7782df
nbodd <nick.bodd@outlook.com>
2018-12-23 10:10:18 -0500

    Updates

---
+++ precache-manifest.adb86ae8958093ef213a2db79c9fc5e3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/crypto-prices/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "602d0c48d4dfead3acbf",
+    "url": "/crypto-prices/static/css/main.4f4bfb3c.chunk.css"
+  },
+  {
+    "revision": "d6ad9823bb989397b9e160eb0aa7bd76",
+    "url": "/crypto-prices/static/media/litecoin.d6ad9823.png"
+  },
+  {
+    "revision": "fb2a34e22fe3fdf45c5b",
+    "url": "/crypto-prices/static/js/1.fb2a34e2.chunk.js"
+  },
+  {
+    "revision": "1adde3cc4b81aac17740",
+    "url": "/crypto-prices/static/js/runtime~main.1adde3cc.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/crypto-prices/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/crypto-prices/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/crypto-prices/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/crypto-prices/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/crypto-prices/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/crypto-prices/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/crypto-prices/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/crypto-prices/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "602d0c48d4dfead3acbf",
+    "url": "/crypto-prices/static/js/main.602d0c48.chunk.js"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/crypto-prices/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/crypto-prices/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/crypto-prices/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/crypto-prices/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/crypto-prices/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/crypto-prices/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/crypto-prices/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "5e24b9624b9f1aa365602a605dd443fe",
+    "url": "/crypto-prices/static/media/reactjs.5e24b962.png"
+  },
+  {
+    "revision": "666935c61ba55417a78f547799062d5e",
+    "url": "/crypto-prices/static/media/reduxjs.666935c6.png"
+  },
+  {
+    "revision": "db039ac4027be4ec55100ece695d691d",
+    "url": "/crypto-prices/static/media/kraken.db039ac4.png"
+  },
+  {
+    "revision": "e67280efaa25dd95704037de24fb5198",
+    "url": "/crypto-prices/static/media/bitcoin.e67280ef.png"
+  },
+  {
+    "revision": "fb2a34e22fe3fdf45c5b",
+    "url": "/crypto-prices/static/css/1.f18ad79e.chunk.css"
+  },
+  {
+    "revision": "e72a927283eb781227d4a2f20b0c7067",
+    "url": "/crypto-prices/index.html"
+  }
+];
